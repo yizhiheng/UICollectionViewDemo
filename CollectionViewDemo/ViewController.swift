b2a2e1cdad24bb28851f677620558dad06c5e3f2
LEO <120039015@qq.com>
2015-08-01 20:55:45 -0400

    Initial Commit

---
+++ CollectionViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewDemo
+//
+//  Created by Zhiheng Yi on 2015-08-01.
+//  Copyright (c) 2015 Zhiheng Yi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
